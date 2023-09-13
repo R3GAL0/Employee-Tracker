117f387e9cd99e15afa8ce97e810b89f9639f4fa
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-09-12 22:08:15 -0400

    added the print db functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const mysql = require('mysql2');;
 
 
 start();
@@ -8,11 +9,50 @@ function start() {
     inquirer
         .prompt([
             {
-              type: 'list',
-              name: 'license',
-              message: 'What kind of license should your project have?',
-              choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+                type: 'list',
+                name: 'license',
+                message: 'What kind of license should your project have?',
+                choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
             }
         ])
-        .then();
-    };
+        .then((data) => {
+            if (data.choices == 'view all departments') {
+                view_depts();
+            } else if (data.choices == 'view all roles') {
+                view_roles();
+            } else if (data.choices == 'view all employees') {
+                view_employ();
+            }
+        });
+};
+
+
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
+
+
+function view_depts() {
+    db.query('SELECT * FROM departments', (err, result) => {
+        console.log(result);
+    });
+};
+
+function view_roles() {
+    db.query('SELECT * FROM roles', (err, result) => {
+        console.log(result);
+    });
+};
+
+function view_employ() {
+    db.query('SELECT * FROM employees', (err, result) => {
+        console.log(result);
+    });
+};
